b213e5050e3e56e3e4e5b1aba3e290a663e4c505
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-11-09 12:02:07 -0300

    MovieFlix Created

---
+++ backend/src/main/java/com/rschlichta/MovieFlix/MovieFlixApplication.java
@@ -0,0 +1,13 @@
+package com.rschlichta.MovieFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFlixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rschlichta/MovieFlix/MovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rschlichta.MovieFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
