709b3da3cfce63b0ebec0bb0dd956936f61f57e2
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-12-21 20:19:26 -0300

    front web home

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles.scss';
+import './style.scss';
 
 const Navbar = () => (
     <nav className="row bg-primary main-nav">
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './style.scss';
 
 const Home = () => (
-    
-    <h1>Home</h1>
+    <div className="home-container" >
+      <h1>Home</h1>      
+    </div>
 );
 
 export default Home;
