fb7d872ed941aeb525eab735c6b13a974c703d56
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2021-12-22 08:20:07 -0300

    Initial Login

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { ReactComponent as MainImage } from '../../core/images/main.svg';
 import './style.scss';
 
 const Home = () => (
-    <div className="home-container" >
-      <h1>Home</h1>      
-    </div>
+
+  <div className = "home-container">
+    <div className = "home-content">
+    <h1 className = "home-title">Avalie Filmes</h1>          
+      <div>
+          <h3 className="home-subTitle">
+              Diga o que você achou do seu filme favorito
+          </h3>
+      </div>
+      <div className="home-image">
+      <MainImage />
+    </div>  
+      </div>
+  </div>
+
+
 );
 
 export default Home;
