7f00c21c65dc79ad5d1c8616c939229a4310c544
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2022-01-01 10:09:56 -0300

    Genre Option

--- front-web/src/pages/Movie/index.tsx
+++ front-web/src/pages/Movie/index.tsx
@@ -18,6 +18,16 @@ const Movie = () => {
         <option value="valor1">AÇÃO</option>
         <option value="valor2">ANIMAÇÃO</option>
         <option value="valor3">AVENTURA</option>
+        <option value="valor1">COMÉDIA</option>
+        <option value="valor2">COMÉDIA ROMÂNTICA</option>
+        <option value="valor3">DOCUMENTÁRIO</option>
+        <option value="valor2">DRAMA</option>
+        <option value="valor3">FICÇÃO CIENTÍFICA</option>
+        <option value="valor1">MIMIZENTO</option>
+        <option value="valor2">MUSICAL</option>
+        <option value="valor3">ROMANCE</option>
+        <option value="valor2">SUSPENSE</option>
+        <option value="valor3">TERROR</option>
         </select>  
 
         <div className = "card-movies">
