7f6d8cb5d5933d1120f3895afe5c1bf5c520ebe7
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2022-01-19 10:40:27 -0300

    netlify

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,8 +1,8 @@
 import jwtDecode from "jwt-decode";
 import history from "./history";
 
-export const CLIENT_ID = 'movieflix';
-export const CLIENT_SECRET = 'movieflix123';
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'movieflix';
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'movieflix123';
 
 type LoginResponse = {
   access_token: string
